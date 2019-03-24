8d79b298802559a165fcac78826f89c43b7f8e88
Luis Colon <luiscolon62@gmail.com>
2019-03-24 19:23:42 -0400

    Initial delivery

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# basic-task-runner
-Basic Gulp task runner I use for quick and dirty websites.
+# Basic Task Runner
+
+Basic Gulp task runner I use for quick and dirty websites - udpated to work with Gulp4.
+
+## Commands
+
+### Build
+
+Deletes css and js files generated during development, minifies, etc.
+
+```
+gulp build
+```
+
+- Css files with be appended with `.min.css`
+- All JS files will be compressed and concatinated to 1 file
+
+### Clean
+
+Deletes css and js files generated during development.
+
+```
+gulp clean
+```
+
+### Watch (Default Task)
+
+Generates the current CSS and JS files and then waits for changes.
+
+```
+gulp
+```
+
+or
+
+```
+gulp watch
+```
+
+### Watchcss
+
+Like watch - but only for CSS files
+
+```
+gulp watchcss
+```
+
+### Watchjs
+
+Like watch - but only for JS files
+
+```
+gulp watchjs
+```
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const { dest, parallel, series, src, watch } = require("gulp");
+const autoprefixer = require('autoprefixer');
+const babel = require('gulp-babel');
+const beeper = require('beeper');
+const cssnano = require('cssnano');
+const del = require('del');
+const log = require('fancy-log');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const uglifyjs = require('gulp-uglifyjs');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+const paths = {
+  scripts: {
+    dest: './static/js',
+    js: './assets/js/**/*.js'
+  },
+  styles: {
+    dest: './static/css',
+    sass: './assets/sass/**/*.scss'
+  }
+};
+
+function deleteDevelopmentCss() {
+  return del([paths.styles.dest]);
+}
+
+function deleteDevelopmentJs() {
+  return del([paths.scripts.dest]);
+}
+
+function errorHandler(error) {
+  log.error(error.message);
+  beeper();
+  this.emit('end');
+}
+
+function generateDevelopmentCss() {
+  return src(paths.styles.sass)
+    .pipe(plumber(errorHandler))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(plumber.stop())
+    .pipe(dest(paths.styles.dest))
+}
+
+function generateProductionCss() {
+  return src(paths.styles.sass)
+    .pipe(plumber(errorHandler))
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(plumber.stop())
+    .pipe(dest(paths.styles.dest))
+}
+
+function transpileDevelopmentJs() {
+  return src(paths.scripts.js)
+    .pipe(plumber(errorHandler))
+    .pipe(sourcemaps.init())
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(plumber.stop())
+    .pipe(dest(paths.scripts.dest))
+}
+
+function transpileProductionJs() {
+  return src(paths.scripts.js)
+    .pipe(plumber(errorHandler))
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(uglifyjs())
+    .pipe(rename('app.js'))
+    .pipe(plumber.stop())
+    .pipe(dest(paths.scripts.dest))
+}
+
+function watchCss() {
+  watch(paths.styles.sass, series(generateDevelopmentCss));
+}
+
+function watchJs() {
+  watch(paths.scripts.js, series(transpileDevelopmentJs));
+}
+
+exports.build = series(deleteDevelopmentCss, deleteDevelopmentJs, generateProductionCss, transpileProductionJs);
+exports.build_dev = series(generateDevelopmentCss, transpileDevelopmentJs);
+exports.clean = parallel(deleteDevelopmentCss, deleteDevelopmentJs);
+exports.default = series(generateDevelopmentCss, transpileDevelopmentJs, parallel(watchCss, watchJs));
+exports.watch = series(generateDevelopmentCss, transpileDevelopmentJs, parallel(watchCss, watchJs));
+exports.watchcss = series(generateDevelopmentCss, watchCss);
+exports.watchjs = series(transpileDevelopmentJs, watchJs);
